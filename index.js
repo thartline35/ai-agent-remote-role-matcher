589849db2df3216c99e7fdb539d34bd04d7c28cf
thartline35 <tammyhartline@gmail.com>
2025-08-04 23:53:35 -0500

    Changed logic from webscraping to using the theirstack api

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
+// Serve static files
+app.use(express.static(__dirname));
+
 // Enhanced timeout middleware with different timeouts for different endpoints
 app.use((req, res, next) => {
     let timeout;
